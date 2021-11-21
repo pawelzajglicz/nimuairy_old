5f600615abcefb2a419541dd3c3e722b3b027d03
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-11-21 23:42:10 +0100

    running backend

---
+++ backend/src/main/java/com/nimuairy/nimuairy/NimuairyApplication.java
@@ -0,0 +1,13 @@
+package com.nimuairy.nimuairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NimuairyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NimuairyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nimuairy/nimuairy/NimuairyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimuairy.nimuairy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimuairyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
