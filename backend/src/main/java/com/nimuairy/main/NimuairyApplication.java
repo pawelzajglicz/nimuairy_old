10817783568eb3375d626c942a9ae7aff4a7fa16
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2021-11-22 18:24:21 +0100

    folder renaming

--- backend/src/main/java/com/nimuairy/main/NimuairyApplication.java
+++ backend/src/main/java/com/nimuairy/main/NimuairyApplication.java
@@ -1,4 +1,4 @@
-package com.nimuairy.nimuairy;
+package com.nimuairy.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/nimuairy/main/NimuairyApplicationTests.java
+++ backend/src/test/java/com/nimuairy/main/NimuairyApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nimuairy.nimuairy;
+package com.nimuairy.main;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
