391fe485b0a0c3e4845f146852e392072cc7626a
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2022-02-23 21:17:35 +0100

    adding eureka server

---
+++ backend/eureka-server/src/test/java/com/nimuairy/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nimuairy.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
