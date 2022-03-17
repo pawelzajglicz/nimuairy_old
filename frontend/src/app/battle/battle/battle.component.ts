c7cecace4e462bcbe03e0f51e317245f6b0c0f0e
Pawel Zajglicz <pawel.zajglicz@gmail.com>
2022-03-17 22:46:42 +0100

    adding BattleComponent in BettleModule and LazyLoading for this

---
+++ frontend/src/app/battle/battle/battle.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nim-battle',
+  templateUrl: './battle.component.html',
+  styleUrls: ['./battle.component.scss']
+})
+export class BattleComponent {
+
+  constructor() { }
+
+}
